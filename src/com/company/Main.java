3a585cc8bf15d337d71c1ce6708937d9887f3f20
mehdi-avara <avaramehdi@gmail.com>
2022-03-29 12:12:29 +0430

    finishup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,11 @@ public class Main {
         linkedList1.add(3);
         System.out.println(linkedList1.size()); // 2
         linkedList1.add(1, 2);
-        Integer list[] = linkedList1.toArray();
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i]+" "); // 1 2 3
+        Integer[] list = linkedList1.toArray();
+        for (Integer integer : list) {
+            System.out.print(integer + " "); // 1 2 3
         }
-        System.out.println("end");
+        System.out.println();
         System.out.println(linkedList1.contains(4)); // false
         System.out.println(linkedList1.indexOf(1)); // 0
         LinkedList linkedList2 = new LinkedList();
